3ed57eb3a6e754908e43766f8f86e2bbf6d22a92
PM-KIRILL <130344954+PM-KIRILL@users.noreply.github.com>
2023-08-30 20:00:39 +0300

    Add files via upload

---
+++ ledcube-PM-Kirill.ino
@@ -0,0 +1,733 @@
+
+  int layer[4]={A3,A2,A1,A0}; //инициализация и порядок слоев (от верха к низу)
+  int column[16]={13,12,11,10,9,8,7,6,5,4,3,2,1,0,A5,A4}; //инициализация и порядок колонн
+  int time = 250; //время
+
+  //В написании скетча помог автор канала (Веселый радиолюбитель)
+ 
+void setup()
+{
+  
+  for(int i = 0; i<16; i++)   
+  {
+    pinMode(column[i], OUTPUT);  //установка колонн
+  }
+  
+  for(int i = 0; i<4; i++)
+  {
+    pinMode(layer[i], OUTPUT);  //установка слоев
+  }
+  
+  randomSeed(analogRead(10));  //создание случайного шаблона
+}
+
+void loop() //программа эффектов
+{
+  turnEverythingOff();
+  flickerOn();
+  turnEverythingOn();
+  delay(time);
+  turnOnAndOffAllByLayerUpAndDownNotTimed();
+  layerstompUpAndDown();
+  spiralInAndOut();
+  turnOnAndOffAllByColumnSideways();
+  delay(time);
+  aroundEdgeDown();
+  turnEverythingOff();
+  randomflicker();
+  randomRain();
+  diagonalRectangle();
+  goThroughAllLedsOneAtATime();
+  propeller();
+  spiralInAndOut();
+  flickerOff();
+  turnEverythingOff();
+  delay(2000);
+}
+
+
+//turn all off
+void turnEverythingOff()
+ {
+   for(int i = 0; i<16; i++)
+   {
+     digitalWrite(column[i], 1);
+   }
+   for(int i = 0; i<4; i++)
+   {
+     digitalWrite(layer[i], 0);
+   }
+ }
+ //Тут были названия эффектов на английском, но я перевел на свой вкус
+//все включить
+void turnEverythingOn()
+{
+  for(int i = 0; i<16; i++)
+  {
+    digitalWrite(column[i], 0);
+  }
+  //включить слои
+  for(int i = 0; i<4; i++)
+  {
+    digitalWrite(layer[i], 1);
+  }
+}
+//выключить колонны
+void turnColumnsOff()
+{
+  for(int i = 0; i<16; i++)
+  {
+    digitalWrite(column[i], 1);
+  }
+}
+//включить мерцание
+void flickerOn()
+{
+  int i = 150;
+  while(i != 0)
+  {
+    turnEverythingOn();
+    delay(i);
+    turnEverythingOff();
+    delay(i);
+    i-= 5;
+  }
+}
+//включение и выключение слоев по очереди
+void turnOnAndOffAllByLayerUpAndDownNotTimed()
+{
+  int x = 75;
+  for(int i = 5; i != 0; i--)
+  {
+    turnEverythingOn();
+    for(int i = 4; i!=0; i--)
+    {
+      digitalWrite(layer[i-1], 0);
+      delay(x);
+    }
+    for(int i = 0; i<4; i++)
+    {
+      digitalWrite(layer[i], 1);
+      delay(x);
+    }
+      for(int i = 0; i<4; i++)
+    {
+      digitalWrite(layer[i], 0);
+      delay(x);
+    }
+    for(int i = 4; i!=0; i--)
+    {
+      digitalWrite(layer[i-1], 1);
+      delay(x);
+    }
+  }
+}
+//включить и выключить по очереди столбиками
+void turnOnAndOffAllByColumnSideways()
+{
+  int x = 75;
+  turnEverythingOff();
+  //включить слои
+  for(int i = 0; i<4; i++)
+  {
+    digitalWrite(layer[i], 1);
+  }
+  for(int y = 0; y<3; y++)
+  {
+    //включить 0-3
+    for(int i = 0; i<4; i++)
+    {
+      digitalWrite(column[i], 0);
+      delay(x);
+    }
+    //включить 4-7
+    for(int i = 4; i<8; i++)
+    {
+      digitalWrite(column[i], 0);
+      delay(x);
+    }
+    //включить 8-11
+    for(int i = 8; i<12; i++)
+    {
+      digitalWrite(column[i], 0);
+      delay(x);
+    }
+    //включить 12-15
+    for(int i = 12; i<16; i++)
+    {
+      digitalWrite(column[i], 0);
+      delay(x);
+    }
+    //выключить 0-3
+    for(int i = 0; i<4; i++)
+    {
+      digitalWrite(column[i], 1);
+      delay(x);
+    }
+    //выключить 4-7
+    for(int i = 4; i<8; i++)
+    {
+      digitalWrite(column[i], 1);
+      delay(x);
+    }
+    //выключить 8-11
+    for(int i = 8; i<12; i++)
+    {
+      digitalWrite(column[i], 1);
+      delay(x);
+    }
+    //выключить 12-15
+    for(int i = 12; i<16; i++)
+    {
+      digitalWrite(column[i], 1);
+      delay(x);
+    }
+    //включить 12-15
+    for(int i = 12; i<16; i++)
+    {
+      digitalWrite(column[i], 0);
+      delay(x);
+    }
+    //включить 8-11
+    for(int i = 8; i<12; i++)
+    {
+      digitalWrite(column[i], 0);
+      delay(x);
+    }
+    //включить 4-7
+    for(int i = 4; i<8; i++)
+    {
+      digitalWrite(column[i], 0);
+      delay(x);
+    }
+    //включить 0-3
+    for(int i = 0; i<4; i++)
+    {
+      digitalWrite(column[i], 0);
+      delay(x);
+    }
+    //выключить 12-15
+    for(int i = 12; i<16; i++)
+    {
+      digitalWrite(column[i], 1);
+      delay(x);
+    }
+    //выключить 8-11
+    for(int i = 8; i<12; i++)
+    {
+      digitalWrite(column[i], 1);
+      delay(x);
+    }
+    //выключить 4-7
+    for(int i = 4; i<8; i++)
+    {
+      digitalWrite(column[i], 1);
+      delay(x);
+    }
+    //выключить 0-3
+    for(int i = 0; i<4; i++)
+    {
+      digitalWrite(column[i], 1);
+      delay(x);
+    }
+  }
+}
+//один слой вверх-вниз
+void layerstompUpAndDown()
+{
+  int x = 75;
+  for(int i = 0; i<4; i++)
+  {
+    digitalWrite(layer[i], 0);
+  }
+  for(int y = 0; y<5; y++)
+  {
+    for(int count = 0; count<1; count++)
+    { 
+      for(int i = 0; i<4; i++)
+      {
+        digitalWrite(layer[i], 1);
+        delay(x);
+        digitalWrite(layer[i], 0);
+      }
+      for(int i = 4; i !=0; i--)
+      {
+        digitalWrite(layer[i-1], 1);
+        delay(x);
+        digitalWrite(layer[i-1], 0);
+      }
+    }
+    for(int i = 0; i<4; i++)
+    {
+      digitalWrite(layer[i], 1);
+      delay(x);
+    }
+    for(int i = 4; i!=0; i--)
+    {
+      digitalWrite(layer[i-1], 0);
+      delay(x);
+    }
+  }
+}
+//выключить мигание
+void flickerOff()
+{
+  turnEverythingOn();
+  for(int i = 0; i!= 150; i+=5)
+  {
+    turnEverythingOff();
+    delay(i+50);
+    turnEverythingOn();
+    delay(i);
+  }
+}
+//по краю куба вниз
+void aroundEdgeDown()
+{
+  for(int x = 200; x != 0; x -=50)
+  {
+    turnEverythingOff();
+    for(int i = 4; i != 0; i--)
+    {
+      digitalWrite(layer[i-1], 1);
+      digitalWrite(column[5], 0);
+      digitalWrite(column[6], 0);
+      digitalWrite(column[9], 0);
+      digitalWrite(column[10], 0);
+      
+      digitalWrite(column[0], 0);
+      delay(x);
+      digitalWrite(column[0], 1);
+      digitalWrite(column[4], 0);
+      delay(x);
+      digitalWrite(column[4], 1);
+      digitalWrite(column[8], 0);
+      delay(x);
+      digitalWrite(column[8], 1);
+      digitalWrite(column[12], 0);
+      delay(x);
+      digitalWrite(column[12], 1);
+      digitalWrite(column[13], 0);
+      delay(x);
+      digitalWrite(column[13], 1);
+      digitalWrite(column[15], 0);
+      delay(x);
+      digitalWrite(column[15], 1);
+      digitalWrite(column[14], 0);
+      delay(x);
+      digitalWrite(column[14], 1);
+      digitalWrite(column[11], 0);
+      delay(x);
+      digitalWrite(column[11], 1);
+      digitalWrite(column[7], 0);
+      delay(x);
+      digitalWrite(column[7], 1);
+      digitalWrite(column[3], 0);
+      delay(x);
+      digitalWrite(column[3], 1);
+      digitalWrite(column[2], 0);
+      delay(x);
+      digitalWrite(column[2], 1);
+      digitalWrite(column[1], 0);
+      delay(x);
+      digitalWrite(column[1], 1);
+    }
+  }
+}
+//случайные огоньки
+void randomflicker()
+{
+  turnEverythingOff();
+  int x = 10;
+  for(int i = 0; i !=750; i+=2)
+  {
+  int randomLayer = random(0,4);
+  int randomColumn = random(0,16);
+  
+  digitalWrite(layer[randomLayer], 1);
+  digitalWrite(column[randomColumn], 0);
+  delay(x);
+  digitalWrite(layer[randomLayer], 0);
+  digitalWrite(column[randomColumn], 1);
+  delay(x); 
+  }
+}
+//случайный дождик
+void randomRain()
+{
+  turnEverythingOff();
+  int x = 100;
+  for(int i = 0; i!=60; i+=2)
+  {
+    int randomColumn = random(0,16);
+    digitalWrite(column[randomColumn], 0);
+    digitalWrite(layer[0], 1);
+    delay(x+50);
+    digitalWrite(layer[0], 0);
+    digitalWrite(layer[1], 1);
+    delay(x);
+    digitalWrite(layer[1], 0);
+    digitalWrite(layer[2], 1);
+    delay(x);
+    digitalWrite(layer[2], 0);
+    digitalWrite(layer[3], 1);
+    delay(x+50);
+    digitalWrite(layer[3], 0);
+    digitalWrite(column[randomColumn], 1);
+  }
+}
+//диагональная крутяшка
+void diagonalRectangle()
+{
+  int x = 350;
+  turnEverythingOff();
+  for(int count = 0; count<5; count++)
+  {
+    //top left
+    for(int i = 0; i<8; i++)
+    {
+      digitalWrite(column[i], 0);
+    }
+    digitalWrite(layer[3], 1);
+    digitalWrite(layer[2], 1);
+    delay(x);
+    turnEverythingOff();
+    //средний средний
+    for(int i = 4; i<12; i++)
+    {
+      digitalWrite(column[i], 0);
+    }
+    digitalWrite(layer[1], 1);
+    digitalWrite(layer[2], 1);
+    delay(x);
+    turnEverythingOff();
+    //нижний правый
+    for(int i = 8; i<16; i++)
+    {
+      digitalWrite(column[i], 0);
+    }
+    digitalWrite(layer[0], 1);
+    digitalWrite(layer[1], 1);
+    delay(x);
+    turnEverythingOff();
+    //нижний средний
+    for(int i = 4; i<12; i++)
+    {
+      digitalWrite(column[i], 0);
+    }
+    digitalWrite(layer[0], 1);
+    digitalWrite(layer[1], 1);
+    delay(x);
+    turnEverythingOff();
+    //нижний левый
+    for(int i = 0; i<8; i++)
+    {
+      digitalWrite(column[i], 0);
+    }
+    digitalWrite(layer[0], 1);
+    digitalWrite(layer[1], 1);
+    delay(x);
+    turnEverythingOff();
+    //средний средний
+    for(int i = 4; i<12; i++)
+    {
+      digitalWrite(column[i], 0);
+    }
+    digitalWrite(layer[1], 1);
+    digitalWrite(layer[2], 1);
+    delay(x);
+    turnEverythingOff();
+    //верхний правый
+    for(int i = 8; i<16; i++)
+    {
+      digitalWrite(column[i], 0);
+    }
+    digitalWrite(layer[2], 1);
+    digitalWrite(layer[3], 1);
+    delay(x);
+    turnEverythingOff();
+    //верхний средний
+    for(int i = 4; i<12; i++)
+    {
+      digitalWrite(column[i], 0);
+    }
+    digitalWrite(layer[2], 1);
+    digitalWrite(layer[3], 1);
+    delay(x);
+    turnEverythingOff();
+  }
+  //верхний левый
+  for(int i = 0; i<8; i++)
+  {
+    digitalWrite(column[i], 0);
+  }
+  digitalWrite(layer[3], 1);
+  digitalWrite(layer[2], 1);
+  delay(x);
+  turnEverythingOff();
+}
+//крутящийся пропеллер
+void propeller()
+{
+  turnEverythingOff();
+  int x = 90;
+  for(int y = 4; y>0; y--)
+  {
+    for(int i = 0; i<6; i++)
+    {
+      //turn on layer
+      digitalWrite(layer[y-1], 1);
+      //a1
+      turnColumnsOff();
+      digitalWrite(column[0], 0);
+      digitalWrite(column[5], 0);
+      digitalWrite(column[10], 0);
+      digitalWrite(column[15], 0);
+      delay(x);
+      //b1
+      turnColumnsOff();
+      digitalWrite(column[4], 0);
+      digitalWrite(column[5], 0);
+      digitalWrite(column[10], 0);
+      digitalWrite(column[11], 0);
+      delay(x);
+      //c1
+      turnColumnsOff();
+      digitalWrite(column[6], 0);
+      digitalWrite(column[7], 0);
+      digitalWrite(column[8], 0);
+      digitalWrite(column[9], 0);
+      delay(x);
+      //d1
+      turnColumnsOff();
+      digitalWrite(column[3], 0);
+      digitalWrite(column[6], 0);
+      digitalWrite(column[9], 0);
+      digitalWrite(column[12], 0);
+      delay(x);
+      //d2
+      turnColumnsOff();
+      digitalWrite(column[2], 0);
+      digitalWrite(column[6], 0);
+      digitalWrite(column[9], 0);
+      digitalWrite(column[13], 0);
+      delay(x);
+      //d3
+      turnColumnsOff();
+      digitalWrite(column[1], 0);
+      digitalWrite(column[5], 0);
+      digitalWrite(column[10], 0);
+      digitalWrite(column[14], 0);
+      delay(x);
+    }
+  }
+  //d4
+  turnColumnsOff();
+  digitalWrite(column[0], 0);
+  digitalWrite(column[5], 0);
+  digitalWrite(column[10], 0);
+  digitalWrite(column[15], 0);
+  delay(x);
+}
+//спираль
+void spiralInAndOut()
+{
+  turnEverythingOn();
+  int x = 60;
+  for(int i = 0; i<6; i++)
+  {
+    //спираль по часовой
+    digitalWrite(column[0], 1);
+    delay(x);
+    digitalWrite(column[1], 1);
+    delay(x);
+    digitalWrite(column[2], 1);
+    delay(x);
+    digitalWrite(column[3], 1);
+    delay(x);
+    digitalWrite(column[7], 1);
+    delay(x);
+    digitalWrite(column[11], 1);
+    delay(x);
+    digitalWrite(column[15], 1);
+    delay(x);
+    digitalWrite(column[14], 1);
+    delay(x);
+    digitalWrite(column[13], 1);
+    delay(x);
+    digitalWrite(column[12], 1);
+    delay(x);
+    digitalWrite(column[8], 1);
+    delay(x);
+    digitalWrite(column[4], 1);
+    delay(x);
+    digitalWrite(column[5], 1);
+    delay(x);
+    digitalWrite(column[6], 1);
+    delay(x);
+    digitalWrite(column[10], 1);
+    delay(x);
+    digitalWrite(column[9], 1);
+    delay(x);
+//спираль против часовой
+    digitalWrite(column[9], 0);
+    delay(x);
+    digitalWrite(column[10], 0);
+    delay(x);
+    digitalWrite(column[6], 0);
+    delay(x);
+    digitalWrite(column[5], 0);
+    delay(x);
+    digitalWrite(column[4], 0);
+    delay(x);
+    digitalWrite(column[8], 0);
+    delay(x);
+    digitalWrite(column[12], 0);
+    delay(x);
+    digitalWrite(column[13], 0);
+    delay(x);
+    digitalWrite(column[14], 0);
+    delay(x);
+    digitalWrite(column[15], 0);
+    delay(x);
+    digitalWrite(column[11], 0);
+    delay(x);
+    digitalWrite(column[7], 0);
+    delay(x);
+    digitalWrite(column[3], 0);
+    delay(x);
+    digitalWrite(column[2], 0);
+    delay(x);
+    digitalWrite(column[1], 0);
+    delay(x);
+    digitalWrite(column[0], 0);
+    delay(x);
+//спираль по часовой
+    digitalWrite(column[0], 1);
+    delay(x);
+    digitalWrite(column[4], 1);
+    delay(x);
+    digitalWrite(column[8], 1);
+    delay(x);
+    digitalWrite(column[12], 1);
+    delay(x);
+    digitalWrite(column[13], 1);
+    delay(x);
+    digitalWrite(column[14], 1);
+    delay(x);
+    digitalWrite(column[15], 1);
+    delay(x);
+    digitalWrite(column[11], 1);
+    delay(x);
+    digitalWrite(column[7], 1);
+    delay(x);
+    digitalWrite(column[3], 1);
+    delay(x);
+    digitalWrite(column[2], 1);
+    delay(x);
+    digitalWrite(column[1], 1);
+    delay(x);
+    digitalWrite(column[5], 1);
+    delay(x);
+    digitalWrite(column[9], 1);
+    delay(x);
+    digitalWrite(column[10], 1);
+    delay(x);
+    digitalWrite(column[6], 1);
+    delay(x);
+//спираль против часовой
+    digitalWrite(column[6], 0);
+    delay(x);
+    digitalWrite(column[10], 0);
+    delay(x);
+    digitalWrite(column[9], 0);
+    delay(x);
+    digitalWrite(column[5], 0);
+    delay(x);
+    digitalWrite(column[1], 0);
+    delay(x);
+    digitalWrite(column[2], 0);
+    delay(x);
+    digitalWrite(column[3], 0);
+    delay(x);
+    digitalWrite(column[7], 0);
+    delay(x);
+    digitalWrite(column[11], 0);
+    delay(x);
+    digitalWrite(column[15], 0);
+    delay(x);
+    digitalWrite(column[14], 0);
+    delay(x);
+    digitalWrite(column[13], 0);
+    delay(x);
+    digitalWrite(column[12], 0);
+    delay(x);
+    digitalWrite(column[8], 0);
+    delay(x);
+    digitalWrite(column[4], 0);
+    delay(x);
+    digitalWrite(column[0], 0);
+    delay(x);
+  }
+}
+//посмотреть на все огоньки
+void goThroughAllLedsOneAtATime()
+{
+  int x = 15;
+  turnEverythingOff();
+  for(int y = 0; y<5; y++)
+  {
+    //0-3
+    for(int count = 4; count != 0; count--)
+    {
+      digitalWrite(layer[count-1], 1);
+      for(int i = 0; i<4; i++)
+      {
+        digitalWrite(column[i], 0);
+        delay(x);
+        digitalWrite(column[i], 1);
+        delay(x);
+      }
+    digitalWrite(layer[count-1], 0);
+    }
+    //4-7
+    for(int count = 0; count < 4; count++)
+    {
+      digitalWrite(layer[count], 1);
+      for(int i = 4; i<8; i++)
+      {
+        digitalWrite(column[i], 0);
+        delay(x);
+        digitalWrite(column[i], 1);
+        delay(x);
+      }
+    digitalWrite(layer[count], 0);
+    }
+    //8-11
+    for(int count = 4; count != 0; count--)
+    {
+      digitalWrite(layer[count-1], 1);
+      for(int i = 8; i<12; i++)
+      {
+        digitalWrite(column[i], 0);
+        delay(x);
+        digitalWrite(column[i], 1);
+        delay(x);
+      }
+    digitalWrite(layer[count-1], 0);
+    }
+    //12-15
+    for(int count = 0; count < 4; count++)
+    {
+      digitalWrite(layer[count], 1);
+      for(int i = 12; i<16; i++)
+      {
+        digitalWrite(column[i], 0);
+        delay(x);
+        digitalWrite(column[i], 1);
+        delay(x);
+      }
+    digitalWrite(layer[count], 0);
+    }
+  }
+}
